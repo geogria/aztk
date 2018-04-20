0d0cf94f5ab1f374e4b4a828ece089daaf9f7171
Pablo <paselem@microsoft.com>
2018-04-20 11:16:54 -0700

    initial work for TICK stack

--- aztk/spark/models/plugins/resource_monitor/start_monitor.sh
+++ aztk/spark/models/plugins/resource_monitor/start_monitor.sh
@@ -2,15 +2,8 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $DIR
 
-echo "InfluxDB password is $INFLUXDB_USER_PASSWORD"
-echo "InfluxDB password is $GF_SECURITY_ADMIN_PASSWORD"
-
-# Install pip requirements
-echo "Install pip requirements"
-pip3 install -r requirements.txt
-
 if  [ "$AZTK_IS_MASTER" = "1" ]; then
-    echo "Create the database and grafana containers"
+    echo "Create docker containers"
     sudo docker-compose up --no-start
     echo "Run the containers"
     sudo docker-compose start
@@ -18,6 +11,11 @@ else
     AZTK_IS_MASTER=0
 fi
 
-echo "Run nodestats in background"
-touch nodestats.out
-python3 nodestats.py > nodestats.out 2>&1 $AZTK_MASTER_IP $AZTK_IS_MASTER $AZ_BATCH_POOL_ID $AZ_BATCH_NODE_ID &
+echo "Install telegraf"
+curl -sL https://repos.influxdata.com/influxdb.key | apt-key add -
+source /etc/lsb-release
+echo "deb https://repos.influxdata.com/${DISTRIB_ID,,} ${DISTRIB_CODENAME} stable" | tee /etc/apt/sources.list.d/influxdb.list
+
+echo "Run telegraf"
+apt-get update && sudo apt-get install telegraf
+telegraf --config etc/telegraf.conf
