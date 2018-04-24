9f188227f59c556ef2dfeb025d32ec2afec7ac0b
Pablo <paselem@microsoft.com>
2018-04-24 15:25:33 -0700

    update start script

--- aztk/spark/models/plugins/resource_monitor/start_monitor.sh
+++ aztk/spark/models/plugins/resource_monitor/start_monitor.sh
@@ -2,20 +2,21 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $DIR
 
-if  [ "$AZTK_IS_MASTER" = "1" ]; then
-    echo "Create docker containers"
-    sudo docker-compose up --no-start
-    echo "Run the containers"
-    sudo docker-compose start
-else
-    AZTK_IS_MASTER=0
-fi
+mkdir /etc/telegraf
+cp ./etc/telegraf.conf /etc/telegraf/telegraf.conf
 
 echo "Install telegraf"
 curl -sL https://repos.influxdata.com/influxdb.key | apt-key add -
 source /etc/lsb-release
 echo "deb https://repos.influxdata.com/${DISTRIB_ID,,} ${DISTRIB_CODENAME} stable" | tee /etc/apt/sources.list.d/influxdb.list
+apt-get update && apt-get install telegraf
+
+if  [ "$AZTK_IS_MASTER" = "true" ]; then
+    echo "Create docker containers"
+    sudo docker-compose up --no-start
+    echo "Run the containers"
+    sudo docker-compose start
+fi
 
 echo "Run telegraf"
-apt-get update && apt-get install telegraf
 telegraf --config ./etc/telegraf.conf &
